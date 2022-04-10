cf84b2606df4f0a175f37525b9e761be1af1f8af
Alice-Mad <alina.chupailo@gmail.com>
2022-04-10 22:54:42 +0300

    Second esson homework

---
+++ homework_02-3.py
@@ -0,0 +1,13 @@
+print('Input numeric #1')
+num1 = float(input())
+print('Input numeric #2')
+num2 = float(input())
+_sum = num1+num2
+print('sum =', num1+num2)
+print('diff =', num1-num2)
+print('ratio =', num1/num2)
+print('prod =', num1*num2)
+ratio_int = num1//num2
+print('ratio int =', ratio_int)
+print('excess =', num1 % num2)
+print('raising =', num1**num2)
---
+++ homework_02-4.py
@@ -0,0 +1,4 @@
+print('Input square side')
+square_side = float(input())
+square_area = square_side**2
+print('square_area =', square_area)
