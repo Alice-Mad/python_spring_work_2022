27d651b033eff5cb3de7661d5edde5cb02d95613
Alice-Mad <alina.chupailo@gmail.com>
2022-05-20 16:19:41 +0300

    sunny day

---
+++ task32.py
@@ -0,0 +1,43 @@
+# # todo: Написать скрипт создания базы данных(ER-модели) TestSystem
+# # Скрипт  create_db.py  должен создавать таблицы, индексы , констрейнты в СУБД PostgresSQL
+# # В задании использовать библиотеку psycopg
+#
+#
+# Ссылка на документацию
+# https://www.psycopg.org/psycopg3/docs/basic/usage.html
+# Для подключения использовать пользователя и базу отличную от postgres
+
+import psycopg
+with psycopg.connect("dbname=postgres_32 user=user_32") as conn:
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE group (
+                  id_group serial PRIMARY KEY,
+                  group_name varchar(10) not null,
+            """)
+        cur.execute("""
+            CREATE TABLE student (
+                id_student serial PRIMARY KEY,
+                id_group integer not null,
+                surname varchar(40) not null,
+                name varchar(30) not null,
+                second name varchar(30) not null,
+                age integer not null
+            """)
+# Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no SQL injections!)
+        cur.execute(
+            "INSERT INTO group (group_name) VALUES (%s)", "1510")
+# Query the database and obtain data as Python objects.
+        cur.execute("SELECT * FROM group")
+        cur.fetchone()
+# Try to create a constraint
+        cur.execute("""
+            ALTER TABLE "profile"
+            ADD CONSTRAINT "user_profile_constraint"
+            FOREIGN KEY("id_user")
+            REFERENCES "User" ("id_user")
+            ON DELETE RESTRICT
+            """)
